95b73b4d12c529eccd9bd5dd1cbf81b4a2067695
Wynd20 <62401156+Wynd20@users.noreply.github.com>
2020-04-19 14:50:13 -0400

    Update checkLogin.php
    This is trying to check ton see if it is in the database already. Not sure if i called our correctly tho.

--- checkLogin.php
+++ checkLogin.php
@@ -1,4 +1,12 @@
 <?php
 $name=$_REQUEST['uname'];
 $password=$_REQUEST['psw'];
+
+ //This is trying to check ton see if it is in the database already. 
+$query = mysql_query("SELECT * FROM users WHERE Username ='$name'");
+if(mysql_num_rows ($query) >0){
+  echo 'It already exists :(';
+}else{
+ mysql_query("Insert INTO users (Username, Password) VALUES ('$name', '$password')") 
+}
 ?>
