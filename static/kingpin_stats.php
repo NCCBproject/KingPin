0ed293c912334b7dffa712ee3a756231b759f30c
Guagadu <guagadumc@ymail.com>
2020-04-20 23:14:12 -0400

    Create kingpin_stats.php

---
+++ static/kingpin_stats.php
@@ -0,0 +1,2 @@
+//will send a post request to update the stats page
+//notes to frontend: have it send data for the number of recent entries to be sent, if null it will have a default value, so don't worry if it is empty at first. Message me when you've updated it. ~Niklas
