6834546f17d346d8295f19fe86ba39848908895e
Guagadu <guagadumc@ymail.com>
2020-04-20 22:41:20 -0400

    Updated setup details

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Bowling Web App.
 Project Setup:
 Put __init__.py in your chosen directory. Create  'static' and a 'templates' subdirectories. All .htm and .html files belong in 'templates', while all other files belong in 'static'. There must also be a valid ssl certificate and key in an 'ssl' subfolder. 
 Use the files in the 'setup' directory to set up the environment. 
-flaskInstall uses apt and pip3 to install necessary packages.
+flaskInstall.sh uses apt and pip3 to install necessary packages. It only partially works, so it is recommended to run the steps individually.
 db_setup.sql is a MySQL script that creates the database. You must manually create a flask user in MySQL.
 
 An SSL certificate and private key must be placed into an 'ssl' directory. The certificate must be called "kingpin.cert" and the key must be called "kingpin.key".
--- setup/flaskInstall.sh
+++ setup/flaskInstall.sh
@@ -1,18 +1,14 @@
 #This will install and run flask and necessary python packages
-#Some functions may not work from the shell script. Those will have comments indicating which they are.
-
-
+#Some functions run unreliably from the script. It is recommended to manually run get commands on any that fail.
 echo "Install Python"
 sudo apt-get install python3
 sudo apt-get install python3-pip
+sudo apt-get install python3-venv
+sudo apt-get install mysql-server
 echo "Install flask and necessary Python packages"
 sudo pip3 install flask
 sudo pip3 install mysql-connector
 echo "Starting Python virtual environment"
 python3 -m venv venv
-
-#create a self-signed certificate with the following line
-#  openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
-
-
+echo "create a self-signed certificate with: openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365"
 echo "To run flask, use 'sudo python3 __init__.py"
