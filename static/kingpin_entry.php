e631fa50008d43805f1e7f52555cfb434fbc2ff7
Wynd20 <62401156+Wynd20@users.noreply.github.com>
2020-04-20 21:48:20 -0400

    updated comments and added third frame in frame 10

--- static/kingpin_entry.php
+++ static/kingpin_entry.php
@@ -1,15 +1,26 @@
 <html>
 <body>
-
+//this file is to set up each of the frames in order to be implemented
+  
 Frame 1: <?php echo $_POST["frame11"]; ?><?php echo $_POST["frame12"]; ?><br>
+  
 Frame 2: <?php echo $_POST["frame21"]; ?><?php echo $_POST["frame22"]; ?><br>
+  
 Frame 3: <?php echo $_POST["frame31"]; ?><?php echo $_POST["frame32"]; ?><br>
+  
 Frame 4: <?php echo $_POST["frame41"]; ?><?php echo $_POST["frame42"]; ?><br>
+  
 Frame 5: <?php echo $_POST["frame51"]; ?><?php echo $_POST["frame52"]; ?><br>
+  
 Frame 6: <?php echo $_POST["frame61"]; ?><?php echo $_POST["frame62"]; ?><br>
+  
 Frame 7: <?php echo $_POST["frame71"]; ?><?php echo $_POST["frame72"]; ?><br>
+  
 Frame 8: <?php echo $_POST["frame81"]; ?><?php echo $_POST["frame82"]; ?><br>
+  
 Frame 9: <?php echo $_POST["frame91"]; ?><?php echo $_POST["frame92"]; ?><br>
-Frame 10: <?php echo $_POST["frame101"]; ?><?php echo $_POST["frame102"]; ?><br>
+  
+Frame 10: <?php echo $_POST["frame01"]; ?><?php echo $_POST["frame02"]; <?php echo $_POST["frame03"];?><br>
+  // frame 10 has 3 because if the user gets two stikes the frame will need 3 inputs
 </body>
 </html> 
