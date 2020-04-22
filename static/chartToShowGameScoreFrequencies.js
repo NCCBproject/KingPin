b21d9cb7ec97f8d1a01b12cd53d74a9a460bf9ed
brockculver <58062070+brockculver@users.noreply.github.com>
2020-04-21 23:09:20 -0400

    Add files via upload

---
+++ static/chartToShowGameScoreFrequencies.js
@@ -0,0 +1,19 @@
+var gamesUnder100
+var gamesUnder200
+var gamesUnder300
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data:data = {
+    datasets: [{
+        data: [gamesUnder100, gamesUnder200, gamesUnder300]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Games under 100',
+        'Games under 200',
+        'Games under 300'
+    ]
+};
+    options: options
+});
