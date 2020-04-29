aa51e8c22f68293fa7fb09a725a5d515139b2582
brockculver <58062070+brockculver@users.noreply.github.com>
2020-04-28 22:49:15 -0400

    Update kingpin_stats.js

--- static/kingpin_stats.js
+++ static/kingpin_stats.js
@@ -370,7 +370,7 @@ var donutChart = new Chart(ctx, {
 		return 0;
 	if(String=='x'||String=='x')
 		return 10;
-	if(String=='1'||String=='2'||String=='3'||String=='4'||String=='5'||String=='6'||String=='7'||String=='8'||String=='9'||String=='0'||)
+	if(String=='1'||String=='2'||String=='3'||String=='4'||String=='5'||String=='6'||String=='7'||String=='8'||String=='9'||String=='0')
 		return parseInt(String);
 		return 210;
 	}
