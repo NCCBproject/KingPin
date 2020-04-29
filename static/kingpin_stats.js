5e34f10c12684adbd135dfc41af4c78b275dd7d0
Wynd20 <62401156+Wynd20@users.noreply.github.com>
2020-04-28 22:42:21 -0400

    Update kingpin_stats.js

--- static/kingpin_stats.js
+++ static/kingpin_stats.js
@@ -297,7 +297,8 @@ var donutChart = new Chart(ctx, {
 			backgroundColor: ['rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'],
+                'rgba(75, 192, 192, 1)',
+		'rgba(16, 233, 85)'],
 			
             
 		
